d921ac801977302a97cbbe6ccbe9f65b22337bac
marcusqjorim <105926371+marcusqjorim@users.noreply.github.com>
2026-01-30 17:14:05 +0300

    Fix route order: admin routes before generic /:id routes for DELETE to work

--- routes/events.routes.js
+++ routes/events.routes.js
@@ -11,15 +11,15 @@ const {
 } = require("../controllers/events.controller");
 const { protect, adminOnly } = require("../middleware/auth.middleware");
 
-// Public routes
-router.get("/", getEvents);
-router.get("/:id", getEvent);
-
-// Admin routes (protected)
+// Admin routes (protected) - MUST come before /:id routes
 router.get("/admin/all", protect, adminOnly, getAllEvents);
 router.post("/", protect, adminOnly, createEvent);
 router.put("/:id", protect, adminOnly, updateEvent);
 router.delete("/:id", protect, adminOnly, deleteEvent);
 router.patch("/:id/featured", protect, adminOnly, toggleFeatured);
+
+// Public routes - come after admin routes
+router.get("/", getEvents);
+router.get("/:id", getEvent);
 
 module.exports = router;
